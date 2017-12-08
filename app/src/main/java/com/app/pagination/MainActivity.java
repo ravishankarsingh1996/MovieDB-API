8b9b73f1fca32218a37d42b18b74d933dfe322de
Jobjira <amit@brain>
2017-12-08 17:53:44 +0530

    Pagination done but need some improvement

--- app/src/main/java/com/app/pagination/MainActivity.java
+++ app/src/main/java/com/app/pagination/MainActivity.java
@@ -1,9 +1,9 @@
 package com.app.pagination;
 
 import android.os.AsyncTask;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -31,19 +31,53 @@ public class MainActivity extends AppCompatActivity {
     List movieList = new ArrayList<>();
     RecyclerView recyclerView;
     MovieAdapter movieAdapter;
-    ProgressBar progressBar;
+    ProgressBar progressBarMainContentLoading, progressBarPaginationLoading;
+    private boolean loading = true;
+    LinearLayoutManager mLayoutManager;
+    int pastVisiblesItems, visibleItemCount, totalItemCount;
+    Integer page=1;
+    Integer result = 0;
+    Movie movie;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        RecyclerView.LayoutManager mLayoutManager =
-                new GridLayoutManager(getApplicationContext(), 2);
+        mLayoutManager = new LinearLayoutManager(getApplicationContext());
         recyclerView.setLayoutManager(mLayoutManager);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar_main_activity);
-        progressBar.setVisibility(View.VISIBLE);
+        progressBarMainContentLoading = (ProgressBar) findViewById(R.id.progressBar_main_activity);
+        progressBarPaginationLoading = (ProgressBar) findViewById(R.id.progressBar_main_activity_pagination);
+        progressBarMainContentLoading.setVisibility(View.VISIBLE);
         new GetJSONData().execute();
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener()
+        {
+            @Override
+            public void onScrolled(RecyclerView recyclerView, int dx, int dy)
+            {
+                if(dy > 0)
+                {
+                    visibleItemCount = mLayoutManager.getChildCount();
+                    totalItemCount = mLayoutManager.getItemCount();
+                    pastVisiblesItems = mLayoutManager.findFirstVisibleItemPosition();
+                    Log.v("...", "Loggggg");
+
+                    if (loading)
+                    {
+                        if ( (visibleItemCount + pastVisiblesItems) == totalItemCount)
+                        {
+                            //loading = false;
+                            Toast.makeText(MainActivity.this, "Last", Toast.LENGTH_LONG).show();
+                            //TODO pagination &  fetch new data
+                            progressBarPaginationLoading.setVisibility(View.VISIBLE);
+                            page=page+1;
+                            result=2;
+                            new GetJSONData().execute();
+                        }
+                    }
+                }
+            }
+        });
     }
 
 
@@ -52,7 +86,89 @@ public class MainActivity extends AppCompatActivity {
         @Override
         protected Integer doInBackground(Void... voids) {
             String urlLink = "http://api.themoviedb.org/3/discover/movie?" +
-                    "api_key=2c0bb3ae2ba96a469724d0c25bd4844e";
+                    "api_key=2c0bb3ae2ba96a469724d0c25bd4844e&page="+page;
+            HttpURLConnection httpsURLConnection;
+
+
+            try {
+                URL url = new URL(urlLink);
+                httpsURLConnection = (HttpURLConnection) url.openConnection();
+                int statusCode = httpsURLConnection.getResponseCode();
+                if (statusCode == 200) {
+                    BufferedReader bufferedReader = new BufferedReader(
+                            new InputStreamReader(httpsURLConnection.getInputStream()));
+                    StringBuilder stringBuilder = new StringBuilder();
+                    String line;
+                    while ((line = bufferedReader.readLine()) != null) {
+                        stringBuilder.append(line);
+                    }
+                    parseJSONData(stringBuilder.toString());
+                    result = 1;
+                } else {
+                    result = 0;
+                }
+
+            } catch (MalformedURLException e) {
+                Log.d("MalformedURLException", e.getLocalizedMessage());
+            } catch (IOException e) {
+                Log.d("IOException", e.getLocalizedMessage());
+                //e.printStackTrace();
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Integer integer) {
+
+            if (integer == 1) {
+                movieAdapter = new MovieAdapter(getApplicationContext(), movieList);
+                recyclerView.setItemAnimator(new SlideInUpAnimator());
+                movieAdapter.notifyDataSetChanged();
+                recyclerView.setAdapter(movieAdapter);
+                progressBarMainContentLoading.setVisibility(View.INVISIBLE);
+
+            } else if (integer == 2){
+                movieAdapter = new MovieAdapter(getApplicationContext(), movieList);
+                recyclerView.setItemAnimator(new SlideInUpAnimator());
+                recyclerView.swapAdapter(movieAdapter, false);
+                progressBarMainContentLoading.setVisibility(View.INVISIBLE);
+                progressBarPaginationLoading.setVisibility(View.GONE);
+            } else
+                Toast.makeText(MainActivity.this, "Failed to load JSON data",
+                        Toast.LENGTH_LONG).show();
+        }
+
+        private void parseJSONData(String s) {
+
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+                JSONArray jsonArray = jsonObject.optJSONArray("results");
+                //movieList = new ArrayList<>();
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject post = jsonArray.getJSONObject(i);
+                    movie = new Movie();
+                    movie.setmStringMovieName(post.optString("original_title"));
+                    movie.setmStringMovieDescription(post.optString("overview"));
+                    movie.setmStringMovieReleaseDate(post.optString("release_date"));
+                    movie.setmStringMoviePicURL(post.optString("poster_path"));
+                    movie.setmStringMovieID(post.optString("id"));
+                    movieList.add(movie);
+                }
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+
+        }
+    }
+    private class GetSecondJSONData extends AsyncTask<Void, Void, Integer> {
+
+        @Override
+        protected Integer doInBackground(Void... voids) {
+            String urlLink = "http://api.themoviedb.org/3/discover/movie?" +
+                    "api_key=2c0bb3ae2ba96a469724d0c25bd4844e&page=2";
             HttpURLConnection httpsURLConnection;
             Integer result = 0;
 
@@ -89,9 +205,11 @@ public class MainActivity extends AppCompatActivity {
             if (integer == 1) {
                 movieAdapter = new MovieAdapter(getApplicationContext(), movieList);
                 recyclerView.setItemAnimator(new SlideInUpAnimator());
+               // movieAdapter.notifyDataSetChanged();
                 recyclerView.setAdapter(movieAdapter);
-                progressBar.setVisibility(View.INVISIBLE);
-            } else
+                progressBarMainContentLoading.setVisibility(View.INVISIBLE);
+                progressBarPaginationLoading.setVisibility(View.GONE);
+            }  else
                 Toast.makeText(MainActivity.this, "Failed to load JSON data",
                         Toast.LENGTH_LONG).show();
         }
@@ -101,11 +219,11 @@ public class MainActivity extends AppCompatActivity {
             try {
                 JSONObject jsonObject = new JSONObject(s);
                 JSONArray jsonArray = jsonObject.optJSONArray("results");
-                movieList = new ArrayList<>();
+                //movieList = new ArrayList<>();
 
                 for (int i = 0; i < jsonArray.length(); i++) {
                     JSONObject post = jsonArray.getJSONObject(i);
-                    Movie movie = new Movie();
+                    movie = new Movie();
                     movie.setmStringMovieName(post.optString("original_title"));
                     movie.setmStringMovieDescription(post.optString("overview"));
                     movie.setmStringMovieReleaseDate(post.optString("release_date"));
